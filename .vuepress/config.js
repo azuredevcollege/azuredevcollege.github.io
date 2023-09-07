b6adc7981ef8d96f37388768e094629640eb426c
Clemens Wältken <waeltken@users.noreply.github.com>
2023-09-07 12:06:34 +0200

    Update config.js

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,4 +1,5 @@
 module.exports = {
   title: 'Azure Developer Colleges',
   description: 'Collection of resources for developers to get started on Azure',
+  patterns: ['**/*.md', '**/*.vue', '!node_modules']
 }
